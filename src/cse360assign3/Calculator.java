fe33767b951837398403ceb96fccdf0b64a6e6c0
JulianC-M <jcruzemi@gmail.com>
2016-02-21 18:38:47 -0700

    Initial version of Assignment 3

---
+++ src/cse360assign3/Calculator.java
@@ -0,0 +1,61 @@
+package cse360assign3;
+
+public class Calculator {
+	/** Total for the calculator value*/
+	private int total;
+	
+	/** Constructor for class calculator, initializing total to zero
+	 * 
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** GetTotal returns the total of the calculator
+	 * 
+	 * @return					Returns to total, initially zero
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** Adds a value to the total of class calculator
+	 * 
+	 * @param value				Value to be added
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/** Subtracts a value from the total
+	 * 
+	 * @param value				Value to be subtracted from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/** Multiplies total by given value
+	 * 
+	 * @param value				Value to be multiplied to total
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/** Divides total by given value
+	 * 
+	 * @param value				Value to be divided into total
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**	Prints the history applied to the total
+	 * 
+	 * @return					Returns history of functions, initially ""
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
