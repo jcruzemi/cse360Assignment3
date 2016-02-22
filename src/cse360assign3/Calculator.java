dcc48118e279bd2d7ce59967be21b708a14d948e
JulianC-M <jcruzemi@gmail.com>
2016-02-21 19:42:45 -0700

    Added functions definitions and tests add, subtract, multiply, divide
    Second Version

--- src/cse360assign3/Calculator.java
+++ src/cse360assign3/Calculator.java
@@ -16,7 +16,7 @@ public class Calculator {
 	 * @return					Returns to total, initially zero
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/** Adds a value to the total of class calculator
@@ -24,7 +24,7 @@ public class Calculator {
 	 * @param value				Value to be added
 	 */
 	public void add (int value) {
-		
+		total = total + value;
 	}
 	
 	/** Subtracts a value from the total
@@ -32,7 +32,7 @@ public class Calculator {
 	 * @param value				Value to be subtracted from total
 	 */
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	
 	/** Multiplies total by given value
@@ -40,7 +40,7 @@ public class Calculator {
 	 * @param value				Value to be multiplied to total
 	 */
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
 	
 	/** Divides total by given value
@@ -48,7 +48,10 @@ public class Calculator {
 	 * @param value				Value to be divided into total
 	 */
 	public void divide (int value) {
-		
+		if (value == 0)
+			total = 0;
+		else
+			total = total / value;
 	}
 	
 	/**	Prints the history applied to the total
--- src/cse360assign3/CalculatorTest.java
+++ src/cse360assign3/CalculatorTest.java
@@ -15,27 +15,84 @@ public class CalculatorTest {
 
 	@Test
 	public void testGetTotal() {
-		fail("Not yet implemented");
+		Calculator temp = new Calculator();
+		assertEquals(0, temp.getTotal());
+		//assuming other functions work
+		temp.add(2);
+		assertEquals(2, temp.getTotal());
+		temp.subtract(3);
+		assertEquals(-1, temp.getTotal());
 	}
 
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		Calculator temp = new Calculator();
+		temp.add(1);
+		temp.add(3);
+		temp.add(0);
+		temp.add(10);
+		assertEquals(14, temp.getTotal());
+		temp.add(-2);
+		assertEquals(12, temp.getTotal());
+		temp.add(2);
+		temp.add(6);
+		assertEquals(20, temp.getTotal());
 	}
 
 	@Test
 	public void testSubtract() {
-		fail("Not yet implemented");
+		Calculator temp = new Calculator();
+		temp.add(1);
+		temp.add(3);
+		temp.add(0);
+		temp.add(10);
+		temp.add(-2);
+		temp.add(2);
+		temp.add(6);
+		temp.subtract(6);
+		assertEquals(14, temp.getTotal());
+		temp.subtract(-1);
+		assertEquals(15, temp.getTotal());
+		temp.subtract(10);
+		temp.subtract(5);
+		assertEquals(0, temp.getTotal());
+		temp.subtract(20);
+		assertEquals(-20, temp.getTotal());
+		
 	}
 
 	@Test
 	public void testMultiply() {
-		fail("Not yet implemented");
+		Calculator temp = new Calculator();
+		temp.add(1);
+		temp.multiply(2);
+		assertEquals(2, temp.getTotal());
+		temp.multiply(1);
+		assertEquals(2,temp.getTotal());
+		temp.multiply(5);
+		assertEquals(10, temp.getTotal());
+		temp.multiply(10);
+		assertEquals(100, temp.getTotal());
+		temp.multiply(0);
+		assertEquals(0, temp.getTotal());
 	}
 
 	@Test
 	public void testDivide() {
-		fail("Not yet implemented");
+		Calculator temp = new Calculator();
+		temp.add(1);
+		temp.divide(0);
+		assertEquals(0, temp.getTotal());
+		temp.add(100);
+		temp.divide(1);
+		assertEquals(100, temp.getTotal());
+		temp.divide(2);
+		assertEquals(50, temp.getTotal());
+		temp.divide(5);
+		assertEquals(10, temp.getTotal());
+		temp.divide(0);
+		assertEquals(0, temp.getTotal());
 	}
 
 	@Test
